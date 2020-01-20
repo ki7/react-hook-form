4873f91f5cd6084f91d3ccfaf6bb535a05f8a378
ki7 <assym.el.mahdy@gmail.com>
2020-01-20 06:10:18 +0100

    use Date instead of performance to generate ids to allow server side rendering

--- src/logic/generateId.ts
+++ src/logic/generateId.ts
@@ -1,6 +1,6 @@
-export default () => {
-  const d = performance.now() * 1000;
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+export default () => {  
+  const d = new Date().getTime();
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16 + d) % 16 | 0;
     return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
   });
